0cf09e6440895188da2c6ea40bf35ecca846c9c4
Hemilly Medeiros <hemilly.santos@fatec.sp.gov.br>
2023-11-15 16:39:59 -0300

    atvd(11): questão 2

--- Atvdd_11.sql
+++ Atvdd_11.sql
@@ -5,3 +5,16 @@ CREATE TABLE log_restaurante (
     nome_procedimento VARCHAR(255)
 );
 
+--1.2 Adicione um procedimento
+CREATE OR REPLACE PROCEDURE total_pedidos_cliente(p_codigo_cliente INTEGER)
+AS
+    total_pedidos INTEGER;
+BEGIN
+    -- Lógica para calcular o total de pedidos
+    SELECT COUNT(*) INTO total_pedidos
+    FROM pedidos
+    WHERE codigo_cliente = p_codigo_cliente;
+
+    RAISE NOTICE 'O cliente possui % pedidos.', total_pedidos;
+END;
+
